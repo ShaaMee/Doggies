9b3c56201fac8b832cf27124668118bde5ba98c8
Alexey Pavlov <laraziel@yandex.ru>
2021-11-04 21:02:45 +0300

    Initial Commit

---
+++ Doggies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Doggies
+//
+//  Created by user on 04.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
