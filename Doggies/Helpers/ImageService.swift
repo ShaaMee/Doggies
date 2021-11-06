f194d31b062f858ada0da789bb2f5ae2a07c5c30
Alexey Pavlov <laraziel@yandex.ru>
2021-11-06 20:58:52 +0300

    Image caching added

--- Doggies/Helpers/ImageService.swift
+++ Doggies/Helpers/ImageService.swift
@@ -10,23 +10,37 @@ import UIKit
 class ImageService {
     static let shared = ImageService()
     
+    var imageCache = NSCache<NSString, UIImage>()
+    
     func image(forURLString urlString: String, completion: @escaping (UIImage?) -> () ) -> URLSessionTask? {
         
-        guard let url = URL(string: urlString) else { return nil}
-        
-        let dataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard error == nil else {
-                print(error!.localizedDescription)
-                return
+        if let cachedImage = imageCache.object(forKey: urlString as NSString) {
+            completion(cachedImage)
+        } else {
+            
+            guard let url = URL(string: urlString) else { return nil}
+            
+            let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad, timeoutInterval: 5)
+            
+            let dataTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                
+                guard error == nil else {
+                    print(error!.localizedDescription)
+                    return
+                }
+                
+                guard let data = data,
+                      let response = response as? HTTPURLResponse,
+                      response.statusCode == 200,
+                      let image = UIImage(data: data)
+                else { return }
+                
+                self.imageCache.setObject(image, forKey: urlString as NSString)
+                completion(image)
             }
-            guard let data = data,
-                  let response = response as? HTTPURLResponse,
-                  response.statusCode == 200,
-                  let image = UIImage(data: data)
-            else { return }
-            completion(image)
+            dataTask.resume()
+            return dataTask
         }
-        dataTask.resume()
-        return dataTask
+        return nil
     }
 }
